79be7a74a417f92a2ff914cf6a9819b378ba034a
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2018-11-09 13:14:51 +0000

    Add mult and add

--- symbol.hpp
+++ symbol.hpp
@@ -8,7 +8,9 @@ class Symbol
     T num;
     T den;
   public:
-    Symbol(T num, T den):num(num),den(den) {};
+    Symbol():num(0),den(0) {};
+    Symbol(T num, T den):num(num),den(den) {
+    };
     ~Symbol() {};
     T getNum() const {return num;};
     T getDen() const {return den;};
@@ -27,7 +29,7 @@ class Symbol
     }
 
     // Move
-    Symbol(Symbol&& x):num(x.getDen()),den(x.getDen())
+    Symbol(Symbol&& x):num(x.getNum()),den(x.getDen())
     {
       std::cout << "MOVE CONST" << std::endl;
     }
@@ -41,4 +43,71 @@ class Symbol
       }
       return *this;
     }
+
+    void operator*=(const T& x)
+    {
+      num *= x;
+    }
+
+    void operator*=(const Symbol& x)
+    {
+      num *= x.getNum();
+      den *= x.getDen();
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Symbol<T>& m)
+    {
+      os << m.getNum() << " / " << m.getDen();
+      return os;
+    };
+
+    // MULTIPLICATION
+    friend Symbol operator*(const Symbol& a, const Symbol& b)
+    {
+      Symbol x(a.getNum() * b.getNum(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator*(const Symbol& a, Symbol&& b)
+    {
+      Symbol x(a.getNum() * b.getNum(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator*(Symbol&& a, const Symbol& b)
+    {
+      Symbol x(a.getNum() * b.getNum(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator*(Symbol&& a, Symbol&& b)
+    {
+      Symbol x(a.getNum() * b.getNum(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    // ADDITION
+    friend Symbol operator+(const Symbol& a, const Symbol& b)
+    {
+      Symbol x(a.getNum()*b.getDen() + b.getNum()*a.getDen(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator+(const Symbol& a, Symbol&& b)
+    {
+      Symbol x(a.getNum()*b.getDen() + b.getNum()*a.getDen(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator+(Symbol&& a, const Symbol& b)
+    {
+      Symbol x(a.getNum()*b.getDen() + b.getNum()*a.getDen(), a.getDen() * b.getDen());
+      return x;
+    };
+
+    friend Symbol operator+(Symbol&& a, Symbol&& b)
+    {
+      Symbol x(a.getNum()*b.getDen() + b.getNum()*a.getDen(), a.getDen() * b.getDen());
+      return x;
+    };
 };
--- tests.cpp
+++ tests.cpp
@@ -16,13 +16,49 @@ void test_2x2()
 {
   Symbol<long> x1(10,5);
   Symbol<long> x2(x1);
-  Symbol<long> x3 = x2;
-
-  Symbol<long> y1(genSymbol((long)20, (long)30));
-  Symbol<long> y2 = genSymbol((long)20, (long)30);
 }
 
+void test_mult()
+{
+  Symbol<long> y2 = genSymbol((long)10, (long)5) * genSymbol((long)5, (long)1);
+
+  if (y2.getNum() != 50 || y2.getDen() != 5)
+  {
+    cout << test_failed << __FUNCTION__ << endl;
+    cout << y2 << endl;
+  }
+
+  Symbol<long> y1 = Symbol<long>(10, 5) * y2;
+
+  if (y1.getNum() != 500 || y1.getDen() != 25)
+  {
+    cout << test_failed << __FUNCTION__ << endl;
+    cout << y1 << endl;
+  }
+}
+
+void test_add()
+{
+
+  Symbol<long> y1 = Symbol<long>(10, 5) * Symbol<long>(10, 5);
+
+  if (y1.getNum() != 100 || y1.getDen() != 25)
+  {
+    cout << test_failed << __FUNCTION__ << endl;
+    cout << y1 << endl;
+  }
+
+  Symbol<long> y2 = genSymbol((long)2, (long)2) + y1;
+
+  if (y2.getNum() != 250 || y2.getDen() != 50)
+  {
+    cout << test_failed << __FUNCTION__ << endl;
+    cout << y2 << endl;
+  }
+}
 void run_tests()
 {
   test_2x2();
+  test_mult();
+  test_add();
 }
